595f3885b5120b8521d0232ddad662a653947452
JhonyGms <jhonatan.garcia.ayala.99@gmail.com>
2023-01-09 00:01:11 -0500

    setPokemon

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { getPokemonDetailsApi } from "../api/pokemon";
 
 function Pokemon(props) {
-  const { navigation, route } = props;
+  const {
+    navigation,
+    route: { params },
+  } = props;
+
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params]);
+
+  if (!pokemon) return null;
+
   return (
     <View>
-      <Text>{route.params.id}</Text>
+      <Text>{pokemon.name}</Text>
     </View>
   );
 }
